6153b8899879a595fb0e955fef1def4cc3c55c6c
Gomez <kevin.gomez@sovos.com>
2020-01-30 17:02:20 -0300

    adding set test

--- src/__tests__/commands.js
+++ src/__tests__/commands.js
@@ -1,13 +1,43 @@
-// const { runCommand } = require('../commands');
-// const  = require('../models/Socket');
-// jest.mock('../socket');
-// const parseCommand = command => command.split(' ');
+const { runCommand } = require('../commands');
+const Socket = require('../models/Socket');
+const memcached = require('../memcached');
+const cache = memcached.getMemcachedInstance();
 
-// test('adds 1 + 2 to equal 3', () => {
-//   const command = 'set foo 3 3 3';
-//   const value = 'hol';
-//   const test = runCommand(parseCommand(command), value);
-//   console.log('mostrado mock', test);
-//   expect(storedMessage).toHaveBeenCalledTimes(1)
-//   expect(3).toBe(3);
-// });
+jest.mock('../models/Socket');
+
+const parseCommand = command => command.split(' ');
+
+beforeEach(() => {
+  Socket.mockClear();
+});
+
+describe('Store Commands', () => {
+  test('Set command bad chunk', () => {
+    const socket = new Socket();
+    const command = 'set foo 3 3 3';
+    const value = 'done';
+    runCommand(socket, parseCommand(command), value);
+    expect(socket.badDataChunk).toHaveBeenCalledTimes(1);
+    expect(socket.storedMessage).not.toHaveBeenCalled();
+    expect(memcached.isKeyStored('foo')).toBe(false);
+  });
+  test('Set command ran successfully', () => {
+    const socket = new Socket();
+    const command = 'set foo 3 3 3';
+    const value = 'say';
+    runCommand(socket, parseCommand(command), value);
+    expect(socket.storedMessage).toHaveBeenCalledTimes(1);
+    expect(socket.badDataChunk).not.toHaveBeenCalled();
+    expect(JSON.stringify(cache)).toMatchSnapshot();  
+    memcached.deleteKeyCache('foo');  
+  });
+  test('Set command with exptime -1 (auto delete)', () => {
+    const socket = new Socket();
+    const command = 'set foo 3 -1 3';
+    const value = 'say';
+    runCommand(socket, parseCommand(command), value);
+    expect(socket.storedMessage).toHaveBeenCalledTimes(1);
+    expect(socket.badDataChunk).not.toHaveBeenCalled();
+    expect(memcached.isKeyStored('foo')).toBe(false);
+  });
+});
